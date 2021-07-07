ffb2f1fa4ffc016fd5344f4c917c630caf7dc03b
Bhavya Jain <74396178+bhavyajain2024@users.noreply.github.com>
2021-07-07 17:21:48 +0530

    Update script.js
    Completed the Drumkit

--- 01 - JavaScript Drum Kit/script.js
+++ 01 - JavaScript Drum Kit/script.js
@@ -1,10 +1,29 @@
-window.addEventListener('keydown', function(e){
+
+
+function playSound(e){
+    
     const audio = this.document.querySelector(`audio[data-key="${e.keyCode}"]`);
+    // .class [attribute selector]
+    const element = this.document.querySelector(`.key[data-key="${e.keyCode}"]`);
     if(!audio){
         console.log('The keystroke is not registered for drum kit');
         return;
     }
     audio.currentTime = 0;
     audio.play();
-    console.log(audio);
-})
+    element.classList.add('playing');
+    
+}
+function removeClass(e){
+    if(e.propertyName == "transform"){
+        this.classList.remove('playing');
+    }
+}
+
+key_all = document.querySelectorAll('.key');
+
+key_all.forEach(element => {
+    element.addEventListener('transitionend', removeClass)
+});
+
+window.addEventListener('keydown', playSound);
